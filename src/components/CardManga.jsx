15def1e3d57c0e8486ff50abf83f5343c80ddd19
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-12 00:10:20 -0300

    Create CardManga.jsx
    Componente carta

---
+++ src/components/CardManga.jsx
@@ -0,0 +1,19 @@
+import '../stylesheets/CardManga.css'
+const CardManga = ({ image, name, price, volume }) => {
+  return (
+    <div className='ms-card-body ms-card-body-franchise'>
+      <div className='ms-card-img-container'>
+        <img className='ms-card-img' src={image} alt='' />
+      </div>
+      <div className='ms-card-info'>
+        <div className='ms-card-title'>{`${name}, tomo ${volume}`}</div>
+        <div className='ms-card-price'>1800 $</div>
+        <div className='ms-card-status'>•en stock</div>
+        <div className='ms-card-button-container'><button className='ms-card-button'>Agregar al carrito</button></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default CardManga
