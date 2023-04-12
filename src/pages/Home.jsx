c1b1e4c33d563c64f41e1e48a8bc80d91443b7c0
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-12 00:13:52 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,26 @@ import img1 from '../images/carousel/img1.png'
 import jujutsuCarouselImg from '../images/carousel/jujutsu.png'
 import jujutsuCarouselImg2 from '../images/carousel/jujutsu2.png'
 
+import GalleryManga from '../components/GalleryManga'
 
 const Home = () => {
+  const TEST_IMG = [jujutsuCarouselImg2, jujutsuCarouselImg, img1]
 
-    const TEST_IMG = [jujutsuCarouselImg2, jujutsuCarouselImg, img1]
+  return (
+    <div className='ms-home-body'>
+      <section className='ms-home-section-small'>
+        <Carousel imgs={TEST_IMG} width='50rem' height='200px' title='Mangas mas vendidos!' />
+        <Carousel imgs={TEST_IMG} width='50rem' height='200px' title='Enterate de las ultimas novedades!' />
+      </section>
+      <section style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
+        <GalleryManga mangaName='Tokyo Ghoul' />
+        <GalleryManga mangaName='Ao' />
+        <GalleryManga mangaName='Tokyo Ghoul' />
+        <GalleryManga mangaName='Ao' />
+      </section>
 
-    return (
-        <div className="ms-home-body">
-            <section className='ms-home-section-small'>
-                <Carousel imgs={TEST_IMG} width={"50rem"} height={"200px"} title={"Mangas mas vendidos!"} />
-                <Carousel imgs={TEST_IMG} width={"50rem"} height={"200px"} title={"Enterate de las ultimas novedades!"} />
-            </section>
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Home
