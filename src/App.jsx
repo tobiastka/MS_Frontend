858c1aceb8628131a8507decea98e06744f43279
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-15 18:39:57 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './stylesheets/app.css'
 import Home from './pages/Home'
 import { Provider } from 'react-redux'
 import store from './store/index'
+import Shop from './pages/Shop'
 function App () {
   return (
     <Provider store={store}>
       <div className='app'>
         <Navbar />
         <Route path='/' component={Home} />
+        <Route path='/shop' component={Shop} />
       </div>
     </Provider>
   )
