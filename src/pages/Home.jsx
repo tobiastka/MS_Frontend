8b0e4523a5b967a141e4092347865b91a6bd91a4
Nai <62731284+tobiastka@users.noreply.github.com>
2023-04-15 18:40:34 -0300

    varias up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
 import Carousel from '../components/Carousel'
 import '../stylesheets/Home.css'
-import img1 from '../images/carousel/img1.png'
-import jujutsuCarouselImg from '../images/carousel/jujutsu.png'
-import jujutsuCarouselImg2 from '../images/carousel/jujutsu2.png'
 
 import GalleryManga from '../components/GalleryManga'
 
 const Home = () => {
-  const TEST_IMG = [jujutsuCarouselImg2, jujutsuCarouselImg, img1]
-
   return (
     <div className='ms-home-body'>
       <section className='ms-home-section-small'>
-        <Carousel imgs={TEST_IMG} width='50rem' height='200px' title='Mangas mas vendidos!' />
-        <Carousel imgs={TEST_IMG} width='50rem' height='200px' title='Enterate de las ultimas novedades!' />
+        <Carousel type='best sellers' width='50rem' height='200px' title='Mangas mas vendidos!' />
+        <Carousel type='news' height='200px' title='Enterate de las ultimas novedades!' />
+      </section>
+      <section>
+        {/* <Carousel imgs={TEST_IMG} width='100rem' height='400px' title='Noticias mas recientes' /> */}
       </section>
       <section style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
         <GalleryManga mangaName='Tokyo Ghoul' />
-        <GalleryManga mangaName='Ao' />
-        <GalleryManga mangaName='Tokyo Ghoul' />
+      </section>
+      <section style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
+        <GalleryManga mangaName='My Hero' />
+      </section>
+      <section style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
         <GalleryManga mangaName='Ao' />
       </section>
 
